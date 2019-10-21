d1adaf7b41bf926dad51a9cd7d9b132c05690e4f
Miguel Cardenas <miguelcarde.3@gmail.com>
2019-10-20 19:14:15 -0500

    add image lazy load

--- src/components/first-section/first-section.js
+++ src/components/first-section/first-section.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import whiteLogo from "../../assets/images/logo-white_BalloonJoy.png";
 
 import "./styles.css";
 
-const FirstSection = () => <div className="main" />;
+const FirstSection = () => (
+  <section className="main">
+    <div className="logo">
+      <LazyLoadImage
+        alt="ballon-joy"
+        height={"70px"}
+        src={whiteLogo}
+        width="70px"
+        effect="blur"
+      />
+    </div>
+    <div className="title"></div>
+    <div className="sub-title"></div>
+    <div className="buttons"></div>
+  </section>
+);
 
 export default FirstSection;
