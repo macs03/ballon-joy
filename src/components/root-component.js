8668cdc3345c8fe9c009c52ba44f427ef6841008
Miguel Cardenas <miguel.cardenas@grability.com>
2020-02-08 18:53:51 -0500

    closing menu after select an option

--- src/components/root-component.js
+++ src/components/root-component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { bubble as Menu } from "react-burger-menu";
 
@@ -13,10 +13,84 @@ import Footer from "./footer/footer";
 
 import "./styles.css";
 
-const RootComponent = () => (
-  <div>
-    <div className="menu-container">
-      <Menu left width={"40%"}>
+const RootComponent = () => {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  const handleStateChange = state => {
+    setisMenuOpen(state.isOpen);
+  };
+
+  const closeMenu = () => {
+    setisMenuOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="menu-container">
+        <Menu
+          left
+          width={"40%"}
+          isOpen={isMenuOpen}
+          onStateChange={state => handleStateChange(state)}
+        >
+          <Link
+            className="menu-item"
+            to="main"
+            data-offset="-45"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Home
+          </Link>
+          <Link
+            className="menu-item"
+            to="services"
+            data-offset="-45"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Our Services
+          </Link>
+          <Link
+            className="menu-item"
+            to="kids-night"
+            data-offset="-45"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Kids Night
+          </Link>
+          <Link
+            className="menu-item"
+            to="gallery"
+            data-offset="-45"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Gallery
+          </Link>
+          <Link
+            className="menu-item"
+            to="contact"
+            data-offset="-45"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Contact
+          </Link>
+        </Menu>
+      </div>
+      <div className="menu-container-responsive">
         <Link
           className="menu-item"
           to="main"
@@ -67,68 +141,16 @@ const RootComponent = () => (
         >
           Contact
         </Link>
-      </Menu>
+      </div>
+      <FirstSection />
+      <Services />
+      <KidsNight />
+      <Gallery />
+      <ToastProvider>
+        <Contact />
+      </ToastProvider>
+      <Footer />
     </div>
-    <div className="menu-container-responsive">
-      <Link
-        className="menu-item"
-        to="main"
-        data-offset="-45"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        Home
-      </Link>
-      <Link
-        className="menu-item"
-        to="services"
-        data-offset="-45"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        Our Services
-      </Link>
-      <Link
-        className="menu-item"
-        to="kids-night"
-        data-offset="-45"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        Kids Night
-      </Link>
-      <Link
-        className="menu-item"
-        to="gallery"
-        data-offset="-45"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        Gallery
-      </Link>
-      <Link
-        className="menu-item"
-        to="contact"
-        data-offset="-45"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        Contact
-      </Link>
-    </div>
-    <FirstSection />
-    <Services />
-    <KidsNight />
-    <Gallery />
-    <ToastProvider>
-      <Contact />
-    </ToastProvider>
-    <Footer />
-  </div>
-);
+  );
+};
 export default RootComponent;
