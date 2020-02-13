9a967f89a7d5b1ccb41762b630f9aa9221a772ba
Miguel Cardenas <miguelcardenas@Miguels-MacBook-Pro.local>
2020-02-12 22:04:15 -0500

    Add new images to gallery from file server

--- src/lib/personal-assets.js
+++ src/lib/personal-assets.js
@@ -9,21 +9,561 @@ import image30 from "../assets/ballonJommy/OGAA5806.JPG";
 
 // assets for gallery
 
-let width;
+const decorations = [
+  "https://i.ibb.co/QJ6J0bM/UXFE2419.jpg",
+  "https://i.ibb.co/Mg9N5sx/unnamed-1.jpg",
+  "https://i.ibb.co/55wYDGT/QNNA3201.jpg",
+  "https://i.ibb.co/wzjtPjY/NHHLE3270.jpg",
+  "https://i.ibb.co/KGNZMrZ/OGAA5806.jpg",
+  "https://i.ibb.co/bgcDFmv/LMPT3982.jpg",
+  "https://i.ibb.co/R0627cv/JZFR5235.jpg",
+  "https://i.ibb.co/dgHYHX5/IVOR3427.jpg",
+  "https://i.ibb.co/5vBN0QG/IMG-E4478.jpg",
+  "https://i.ibb.co/t8FzTNv/IMG-E4339.jpg",
+  "https://i.ibb.co/10y8Y4k/IMG-E4134.jpg",
+  "https://i.ibb.co/K0KHH8p/IMG-E2104.jpg",
+  "https://i.ibb.co/8MM9WFc/IMG-E1859.jpg",
+  "https://i.ibb.co/k5cwrPc/IMG-9947.jpg",
+  "https://i.ibb.co/xYbZLMz/IMG-7057.jpg"
+];
 
-if (window.matchMedia("(min-width: 500px)").matches) {
-  width = 4;
-} else {
-  width = 2;
-}
+const bouquets = [
+  "https://i.ibb.co/19n1yf0/YIQB8506.jpg",
+  "https://i.ibb.co/rwPKcn9/XYMH4333.jpg",
+  "https://i.ibb.co/9tZ3Lvf/WOUN4164.jpg",
+  "https://i.ibb.co/qYNXhZx/VNMJ0987.jpg",
+  "https://i.ibb.co/3y3qg5t/VDEC0135.jpg",
+  "https://i.ibb.co/6RV0Qys/unnamedlll-m.jpg",
+  "https://i.ibb.co/FnMnwRs/unnamed.jpg",
+  "https://i.ibb.co/GWTyKDN/unnamed-1-l.jpg",
+  "https://i.ibb.co/rFKJZq6/UMSI6199.jpg",
+  "https://i.ibb.co/ypghJVD/sssd.jpg",
+  "https://i.ibb.co/n3ngHZ0/SFNG9118.jpg",
+  "https://i.ibb.co/bWMQr2b/sdkkd.jpg",
+  "https://i.ibb.co/wNZZXyp/NMSE3651.jpg",
+  "https://i.ibb.co/T4bv0vm/n-lm.jpg",
+  "https://i.ibb.co/LYXYzwK/ncmc.jpg"
+];
+
+const tattoos = [
+  "https://i.ibb.co/pfZKBq4/WZMN3781.jpg",
+  "https://i.ibb.co/R9WZp1L/unnamed-2.jpg",
+  "https://i.ibb.co/Yb7gz5m/UCUI3739.jpg",
+  "https://i.ibb.co/hfLckkF/LQLY1750.jpg",
+  "https://i.ibb.co/kctK2VV/IMG-E4560.jpg",
+  "https://i.ibb.co/3mTJLfP/IMG-E0043.jpg",
+  "https://i.ibb.co/ZdxGkmq/IMG-4961.jpg",
+  "https://i.ibb.co/stTVVWk/IMG-4960.jpg",
+  "https://i.ibb.co/mSPCCmK/IMG-4957.jpg",
+  "https://i.ibb.co/d5Q7W0c/IMG-4956.jpg",
+  "https://i.ibb.co/y4kJ6dr/IMG-4555.jpg",
+  "https://i.ibb.co/zfSKqLm/IMG-4548.jpg",
+  "https://i.ibb.co/B648PFG/IMG-4544.jpg",
+  "https://i.ibb.co/BzKN69v/IMG-4535.jpg",
+  "https://i.ibb.co/yVCH9s6/IMG-2088.jpg",
+  "https://i.ibb.co/9qHzSBG/IMG-4533.jpg"
+];
+
+const painting = [
+  "https://i.ibb.co/hRfkFjM/unnamed.jpg",
+  "https://i.ibb.co/nBfKHQG/unnamed-1.jpg",
+  "https://i.ibb.co/Wx8Hsk9/unnamed-1.jpg",
+  "https://i.ibb.co/K7vS186/TFXU5648.jpg",
+  "https://i.ibb.co/L6XkkPQ/SRHX3975.jpg",
+  "https://i.ibb.co/Qm26cqR/sdsdm.jpg",
+  "https://i.ibb.co/z8PXZmH/NTZL5974.jpg",
+  "https://i.ibb.co/N9MWTFk/nmnm.jpg",
+  "https://i.ibb.co/mtbzkHC/mlk.jpg",
+  "https://i.ibb.co/98Dn8dt/LNOB1943.jpg",
+  "https://i.ibb.co/Fh7gpwX/lllllm.jpg",
+  "https://i.ibb.co/qBCkjgN/JBKR2463.jpg",
+  "https://i.ibb.co/1JcXhwN/IMG-E5426.jpg",
+  "https://i.ibb.co/yR5Mbf6/IMG-E2749.jpg",
+  "https://i.ibb.co/KXj9vhN/IMG-E0707.jpg"
+];
+
+const twisting = [
+  "https://i.ibb.co/1sKz5N5/XBPI1980.jpg",
+  "https://i.ibb.co/tMb9t8Q/VGWJ1152.jpg",
+  "https://i.ibb.co/GkZQmNV/unnamedll.jpg",
+  "https://i.ibb.co/pbn0fQ3/unnamed.jpg",
+  "https://i.ibb.co/xD3k44s/TVMA7350.jpg",
+  "https://i.ibb.co/gRvNS9H/QMXP8538.jpg",
+  "https://i.ibb.co/3r2C37Z/OEEY2570.jpg",
+  "https://i.ibb.co/pwTsqLQ/llllmmn.jpg",
+  "https://i.ibb.co/kKXQVZn/KSXD8849.jpg",
+  "https://i.ibb.co/kKVDm4z/JYRZ7351.jpg",
+  "https://i.ibb.co/JQD3gnQ/JJQL3742.jpg",
+  "https://i.ibb.co/yQ8Zvbs/IYPM2147.jpg",
+  "https://i.ibb.co/w08D65K/INNP9529.jpg",
+  "https://i.ibb.co/6tv96J1/IMG-E9427.jpg",
+  "https://i.ibb.co/WfdgnYL/IMG-E9424.jpg"
+];
 
 const photos = [
-  // {
-  //   src: image19,
-  //   width,
-  //   height: 3,
-  //   category: "bouquets"
-  // }
+  {
+    src: decorations[0],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[1],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[2],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[3],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[4],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[5],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[6],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[7],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[8],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[9],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[10],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[11],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[12],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[13],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+  {
+    src: decorations[14],
+    width: 4,
+    height: 3,
+    category: "decorations"
+  },
+
+  //  bouquets
+  {
+    src: bouquets[0],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[1],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[2],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[3],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[4],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[5],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[6],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[7],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[8],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[9],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[10],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[11],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[12],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[13],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+  {
+    src: bouquets[14],
+    width: 4,
+    height: 3,
+    category: "bouquets"
+  },
+
+  // tattoos
+  {
+    src: tattoos[0],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[1],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[2],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[3],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[4],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[5],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[6],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[7],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[8],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[9],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[10],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[11],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[12],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[13],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[14],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  {
+    src: tattoos[15],
+    width: 4,
+    height: 3,
+    category: "tattoos"
+  },
+  // paiting
+  {
+    src: painting[0],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[1],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[2],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[3],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[4],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[5],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[6],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[7],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[8],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[9],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[10],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[11],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[12],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[13],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+  {
+    src: painting[14],
+    width: 4,
+    height: 3,
+    category: "painting"
+  },
+
+  // twisting
+  {
+    src: twisting[0],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[1],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[2],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[3],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[4],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[5],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[6],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[7],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[8],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[9],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[10],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[11],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[12],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[13],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  },
+  {
+    src: twisting[14],
+    width: 4,
+    height: 3,
+    category: "twisting"
+  }
 ];
 
 export const anotherImages = {
