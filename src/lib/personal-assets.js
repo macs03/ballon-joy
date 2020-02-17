a5cb23dccdc029127c9ece15c03ab99047b4eeed
Miguel Cardenas <miguelcardenas@Miguels-MacBook-Pro.local>
2020-02-17 18:40:21 -0500

    add new assets for services

--- src/lib/personal-assets.js
+++ src/lib/personal-assets.js
@@ -1,8 +1,8 @@
 // assets for web
-import image19 from "../assets/ballonJommy/4c5a8bb3-e306-4ab6-a0f8-8f75bb73c48f.JPG";
-import image23 from "../assets/ballonJommy/5b6c55bf-d56d-4026-8db0-63dadec9b85c.JPG";
+import image19 from "../assets/ballonJommy/face-painting.png";
+import image23 from "../assets/ballonJommy/henna.png";
 import image24 from "../assets/ballonJommy/e3c01776-516a-4ab7-b062-8522ccc2ef11.JPG";
-import image26 from "../assets/ballonJommy/e50bca2f-9dc6-4798-a8a7-29d428d3994b.JPG";
+import image26 from "../assets/ballonJommy/tattoo.png";
 
 import image29 from "../assets/ballonJommy/YIQB8506.JPG";
 import image30 from "../assets/ballonJommy/OGAA5806.JPG";
